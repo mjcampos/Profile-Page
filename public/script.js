c6580915b303979efacc31f80500688160674645
Michael Campos <camposmj89@gmail.com>
2016-01-21 17:30:02 -0800

    Removed MEAN stack content and simplified to static pages

--- public/script.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', ['ngRoute']);
-
-// Clientside routing
-myApp.config(function($routeProvider) {
-	$routeProvider
-	.when('/', {templateUrl: 'partials/home.html'})
-	.when('/projects', {templateUrl: 'partials/projects.html'})
-	.otherwise({redirectTo: '/'});
-})
