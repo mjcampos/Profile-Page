8d016ca8ca6366f2c1e1d8f987180f7e91ec6c1b
Michael Campos <camposmj89@gmail.com>
2016-02-24 22:08:26 -0800

    Added javascript to make site more interactive. This includes a div with some fun facts about myself.

---
+++ assets/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	var speed = 500;
+
+	var getTodaysDay = function() {
+		switch(new Date().getDay()) {
+			case 0:
+				return "Sunday";
+			case 1:
+				return "Monday";
+			case 2:
+				return "Tuesday";
+			case 3:
+				return "Wednesday";
+			case 4:
+				return "Thursday";
+			case 5:
+				return "Friday";
+			case 6:
+				return "Saturday";
+		}
+	}
+
+	var funFact = function() {
+		switch(new Date().getDay()) {
+			case 0:
+				return "I'm first generation Peruvian-American";
+			case 1:
+				return "As a child I had a pet guinea pig named Richard who was brought all the way from Peru";
+			case 2:
+				return "I've been to Germany three times and am in the process of learning German";
+			case 3:
+				return "Although a student of computer science since my parents bought me a computer I'm  a history lover and have memorized the US Presidents after one of my elementary school teacher issued a challenge";
+			case 4:
+				return "My first night in France was spent on the beach of Corsica outside the town of Bastia";
+			case 5:
+				return "I have a motorcycle license and aspires to own my own motorcycle to travel the world with my German friend";
+			case 6:
+				return "My favorite author will forever be the great Kurt Vonnegut";
+		}
+	}
+
+	$(".alert-info")
+		.click(function() {
+			$(this).fadeOut(speed);
+		})
+		.html("<p>Fun Fact For " + getTodaysDay() +":</p>" + "<p class='body'>" + funFact() + "</p>" + "<p class='footer'>(Click to fade away)</p>");
+})
